6d566319d32c3e07d01bb0277504d075bb584c64
45degree <1098902652@qq.com>
2025-02-17 23:29:40 +0800

    [WIP] build by xmake

--- source/xmake.lua
+++ source/xmake.lua
@@ -47,7 +47,7 @@ target('slang', function()
     set_kind('static')
     add_defines('SLANG_STATIC')
   end
-  add_deps('slang-cpp-extractor', 'slang-lookup-generator', 'slang-spirv-embed-generator')
+  add_deps('slang-generate-prelude', 'slang-generate-lookup-tables', 'slang-reflect')
   add_includedirs('slang', '$(projectdir)/build')
 
   add_defines('SLANG_EMBED_CORE_MODULE')
@@ -55,33 +55,12 @@ target('slang', function()
   add_files('slang/*.cpp')
   add_files('slang-record-replay/record/*.cpp')
   add_files('slang-record-replay/util/*.cpp')
-  add_rules('slang-reflect', 'slang-capability-generator-header', 'slang-meta-generate', 'slang-generate-prelude', "slang-generate-core-module-header")
-  add_configfiles('$(projectdir)/slang-tag-version.h.in')
-
-  add_files(
-    'slang/slang-ast-support-types.h',
-    'slang/slang-ast-base.h',
-    'slang/slang-ast-decl.h',
-    'slang/slang-ast-expr.h',
-    'slang/slang-ast-modifier.h',
-    'slang/slang-ast-stmt.h',
-    'slang/slang-ast-type.h',
-    'slang/slang-ast-val.h',
-    { rule = 'slang-reflect' }
-  )
-
   add_files('slang/*.meta.slang', { rule = 'slang-meta-generate' })
   add_files('slang/*.capdef', { rule = 'slang-capability-generator-header' })
-  add_files('$(buildir)/slang-lookup-tables-generates/*.cpp', { always_added = true })
+  add_rules('slang-capability-generator-header', 'slang-meta-generate')
+  add_configfiles('$(projectdir)/slang-tag-version.h.in')
+
   add_files('slang-core-module/*.cpp')
-  add_files('$(projectdir)/prelude/*-prelude.h', { rule = 'slang-generate-prelude' })
-
-  before_build(function(target)
-    import('core.project.task')
-    import('core.project.config')
-    task.run('generate-slang-lookup-tables', {}, path.join(config.buildir(), 'slang-lookup-tables-generates'))
-  end)
-
   add_packages('spirv-headers', 'miniz', 'lz4', 'unordered_dense')
 end)
 
@@ -96,18 +75,24 @@ end)
 
 --- rules and tasks
 
-task('generate-slang-lookup-tables', function()
-  ---@param generate_dir string
-  on_run(function(generate_dir)
-    import('core.project.project')
+target('slang-generate-lookup-tables', function()
+  set_kind('object')
+  add_deps('slang-lookup-generator', 'slang-spirv-embed-generator')
 
-    ---@type Package
-    local spirv = project.required_packages()['spirv-headers']
+  on_load(function(target)
+    target:add('files', path.join(target:autogendir(), 'slang-lookup-tables-generates/*.cpp'), { always_added = true })
+  end)
+
+  ---@param target Target
+  before_build(function(target)
+    local spirv = target:pkgs()['spirv-headers']
     local spirv_include_dir = path.join(spirv:installdir(), 'include')
     local input_json = path.join(spirv_include_dir, 'spirv/unified1/extinst.glsl.std.450.grammar.json')
 
+    local generate_dir = path.join(target:autogendir(), 'slang-lookup-tables-generates')
+
     ---@type Target
-    local slang_lookup_generator = project.target('slang-lookup-generator')
+    local slang_lookup_generator = target:dep('slang-lookup-generator')
 
     local cmd_opts = { input_json }
     table.insert(cmd_opts, path.join(generate_dir, 'slang-lookup-GLSLstd450.cpp'))
@@ -122,83 +107,31 @@ task('generate-slang-lookup-tables', function()
     local output_file = path.join(generate_dir, 'slang-spirv-core-grammar-embed.cpp')
 
     ---@type Target
-    local slang_spirv_embed_generator = project.target('slang-spirv-embed-generator')
+    local slang_spirv_embed_generator = target:dep('slang-spirv-embed-generator')
     cmd_opts = { input_json, output_file }
     os.vrunv(slang_spirv_embed_generator:targetfile(), cmd_opts)
   end)
+
+  add_packages('spirv-headers', 'unordered_dense')
 end)
 
--- rule("generate-slang-lookup-tables", function()
---   ---@param target Target
---   on_load(function(target)
---     target:add("deps", "slang-lookup-generator")
---   end)
---
--- 	before_buildcmd_file(function(target, batchcmds, sourcebatch, opt)
--- 		import("core.project.project")
---
--- 		---@type Package
--- 		local spirv = project.required_packages()["spirv-headers"]
--- 		local spirv_include_dir = path.join(spirv:installdir(), "include")
--- 		local input_json = path.join(spirv_include_dir, "spirv/unified1/extinst.glsl.std.450.grammar.json")
---
--- 		---@type Target
--- 		local slang_lookup_generator = target:dep("slang-lookup-generator"):targetfile()
---
--- 		local cmd_opts = { input_json }
--- 		table.insert(cmd_opts, path.join(generate_dir, "slang-lookup-GLSLstd450.cpp"))
--- 		table.insert(cmd_opts, "GLSLstd450")
--- 		table.insert(cmd_opts, "GLSLstd450")
--- 		table.insert(cmd_opts, "spirv/unified1/GLSL.std.450.h")
---
--- 		batchcmds:show_progress(opt.progress, "${color.build}generating.slang-lookup-tables")
--- 		os.mkdir(generate_dir)
--- 		os.vrunv(slang_lookup_generator:targetfile(), cmd_opts)
--- 	end)
--- end)
+target('slang-reflect', function()
+  set_kind('headeronly')
 
-rule('slang-generate-core-module-header', function()
-  ---@param target Target
-  on_load(function(target)
-    target:add('deps', 'slang-bootstrap')
-    local base_dir = path.join(target:autogendir(), 'core-module-generated-header')
-    target:add('includedirs', base_dir)
-  end)
+  after_load(function(target)
+    local files = {
+      path.join(os.scriptdir(), 'slang/slang-ast-support-types.h'),
+      path.join(os.scriptdir(), 'slang/slang-ast-base.h'),
+      path.join(os.scriptdir(), 'slang/slang-ast-decl.h'),
+      path.join(os.scriptdir(), 'slang/slang-ast-expr.h'),
+      path.join(os.scriptdir(), 'slang/slang-ast-modifier.h'),
+      path.join(os.scriptdir(), 'slang/slang-ast-stmt.h'),
+      path.join(os.scriptdir(), 'slang/slang-ast-type.h'),
+      path.join(os.scriptdir(), 'slang/slang-ast-val.h'),
+    }
 
-  before_buildcmd_file(function(target, batchcmds, sourcefile, opt)
-    local base_dir = path.join(target:autogendir(), 'core-module-generated-header')
-    local header_file = path.join(base_dir, 'slang-core-module-generated.h')
-
-    local cmd_opts = {}
-    table.insert(cmd_opts, '-archive-type')
-    table.insert(cmd_opts, 'riff-lz4')
-    table.insert(cmd_opts, '-save-core-module-bin-source')
-    table.insert(cmd_opts, header_file)
-
-    batchcmds:show_progress(opt.progress, '${color.build.object}generating.slang-core-module-header')
-    batchcmds:mkdir(base_dir)
-    batchcmds:vrunv(target:dep('slang-bootstrap'):targetfile(), cmd_opts)
-  end)
-end)
-
-rule('slang-reflect', function()
-  on_load(function(target)
-    target:add('deps', 'slang-cpp-extractor')
-    local base_dir = path.join(target:autogendir(), 'ast-reflect')
-    target:add('includedirs', base_dir)
-  end)
-
-  ---comment
-  ---@param target Target
-  ---@param batchcmds BatchCommand
-  ---@param sourcebatch SourceBatch
-  ---@param opt TargetOpt
-  before_buildcmd_files(function(target, batchcmds, sourcebatch, opt)
-    local base_dir = path.join(target:autogendir(), 'ast-reflect')
-
-    -- table.insert(target:objectfiles(), objectfile)
-
-    local cmd_opts = sourcebatch.sourcefiles
+    local base_dir = target:autogendir()
+    local cmd_opts = files
     table.insert(cmd_opts, '-strip-prefix')
     table.insert(cmd_opts, 'slang-')
     table.insert(cmd_opts, '-o')
@@ -207,13 +140,14 @@ rule('slang-reflect', function()
     table.insert(cmd_opts, '-mark-suffix')
     table.insert(cmd_opts, '_CLASS')
 
-    batchcmds:show_progress(opt.progress, '${color.build.object}compiling.slang-reflect-generator-header')
-    batchcmds:mkdir(base_dir)
-    batchcmds:vrunv(target:dep('slang-cpp-extractor'):targetfile(), cmd_opts)
-    -- batchcmds:compile(generator_files, objectfile)
+    os.mkdir(base_dir)
+    os.vrunv(target:dep('slang-cpp-extractor'):targetfile(), cmd_opts)
 
-    batchcmds:add_depfiles(sourcefile, target:dep('slang-cpp-extractor'):targetfile())
+    target:add('headerfiles', path.join(base_dir, 'slang-generated', '*.h'))
+    target:add('includedirs', base_dir, { public = true })
   end)
+
+  add_deps('slang-cpp-extractor')
 end)
 
 rule('slang-capability-generator-header', function()
@@ -276,12 +210,20 @@ rule('slang-meta-generate', function()
   end)
 end)
 
+target('slang-generate-prelude', function()
+  set_kind('object')
+  add_rules('slang-generate-prelude')
+
+  add_files('$(projectdir)/prelude/*-prelude.h', { rule = 'slang-generate-prelude' })
+  add_packages('unordered_dense')
+end)
+
 rule('slang-generate-prelude', function()
   on_load(function(target)
     target:add('deps', 'slang-embed')
   end)
 
-  on_buildcmd_file(function(target, batchcmds, sourcefile, opt)
+  before_buildcmd_file(function(target, batchcmds, sourcefile, opt)
     local slang_embed = target:dep('slang-embed'):targetfile()
     local base_dir = path.join(target:autogendir(), 'slang-prelude-generate')
     local sourcefile_cx = path.join(base_dir, path.basename(sourcefile) .. '.cpp')
